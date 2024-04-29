d6ac5407cfb0d619cec26543877e54c17d76998f
guibatistella89 <guibatistella89@gmail.com>
2024-04-29 17:47:33 -0300

    quase finalizado

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Desafio-DWeb-1
 
-Teste
+Teste 
+Passos para enviar ao git.
+1- git add .(esta pegando todos os arquivos que foram editados)
+2- git commit -m "sua mensagem (agora sim!)"
+3- git push
+4- ir no gitHub ver se deu certo
+eSTOU TESTANDO OS COMMITS
---
+++ javascript.js
@@ -0,0 +1,7 @@
+function entrar(){
+    let username = document.getElementById('username').value
+    let password = document.getElementById('password').value
+
+    console.log("Nome do usuário: " + username)
+    console.log("Senha do usuário: " + password)
+}
